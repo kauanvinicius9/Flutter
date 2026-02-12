355d55c322479a0a0314163a2dc5fadf8c9696a3
KAUAN VINÍCIUS SALES SILVA FRANCISCHINI <kauan.francischini@senaisp.edu.br>
2026-02-12 09:05:24 -0300

    up2

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,36 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     // Traz os componentes da biblioteca pra sua tela
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(title:Text("Página principal")
+      home: Scaffold(
+        backgroundColor: const Color(0x00FFFFFF),
+        appBar: AppBar(title: Text("Página Principal", style: TextStyle(fontSize: 30, color: const Color(0xFFFFFFFF))),
+
+        // Centralização. booleano
+        centerTitle: true,
+        backgroundColor: const Color(0xFF460673),
 
         ),
-        body: Column(children: [Text("Texto em coluna"), 
-        Text("KAKAKAKAKAKAK"),
-        TextButton(
-          onPressed: () {
-            print("Parabéns você está milionário");
-          },
-          child: Text("Clica aqui e ganhe 1 milhão de reais"),
+        body: Center(
 
-        )],)
+
+
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 30,
+            children: [
+              Text("Texto em coluna", style: TextStyle(fontSize: 30)),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 233, 219, 243),
+                ),
+                onPressed: () {},
+                child: Text("Isso é um botão"),),
+              
+            ],
+          ),
+        ),
       ),
-    ); 
+    );
   }
 }
